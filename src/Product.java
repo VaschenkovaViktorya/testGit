863bf325309f0fd9899fc54b882e3f289488bc77
gorvi <gorvica@mail.ru>
2022-01-13 22:24:06 +0700

    added Product

---
+++ src/Product.java
@@ -0,0 +1,12 @@
+public class Product {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
+}
